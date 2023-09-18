915fc2e39efccf2bfd54d239495b2b78ceade24c
Vladislav Kuzmenko <vladnau89@yandex.ru>
2023-09-18 14:24:30 +0300

    super optimal

--- Runtime/Bindings/CollectionViewBinding/CollectionViewBinding.cs
+++ Runtime/Bindings/CollectionViewBinding/CollectionViewBinding.cs
@@ -12,31 +12,5 @@ namespace SM.Core.Unity.UI.MVVM
 {
 	public abstract class CollectionViewBinding<TCollection>
 	{
-		private readonly struct Item
-		{
-        }
-
-		[MaybeNull]
-		[field: SerializeField]
-		private string SelectedPropertyName { get; set; }
-
-		[MaybeNull]
-		private object SelectedValue
-		{
-			get => null;
-			set
-			{
-				
-			}
-		}
-
-		[MaybeNull]
-		public object SelectedItem { get; private set; }
-
-		private List<Item> Items { get; } = new();
-
-		public event EventHandler SelectedItemChanged;
-        
-
 	}
 }
